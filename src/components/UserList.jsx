6081bc9617aa36d319ade126d78d06ac25a4ed0e
André <andreglock@gmail.com>
2021-07-14 14:33:12 +0200

    added very basic styling and more info

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './userList.css';
 
 export default class UserList extends React.Component {
     constructor () {
@@ -16,16 +17,22 @@ export default class UserList extends React.Component {
         </>
     }
     
-    RenderList = () => <ul className="userList">{this.state.users.map(e => <li>{e.id}: {e.name}</li>)}</ul>;
+    RenderList = () => <ul className="userList">{this.state.users.map(e => <li>
+            {e.id}: {e.name} - Email: {e.email}<br></br>
+            Phone Number: {e.phone} <br></br>
+            Street: {e.address.street}/ Suite: {e.address.suite} <br></br>
+            City: {e.address.city}/ Zip Code: {e.address.zipcode}
+        </li>)}</ul>;
 
     LoadUser = () => {
         fetch(`https://jsonplaceholder.typicode.com/users`, {
             method: "GET",
         })
             .then(response => response.json())
-            .then(result => this.setState({
-                users: result
-            }))
+            .then(result => {
+                this.setState({ users: result })
+                console.log(result)
+            })
             .catch(e => console.warn("whops\nError:", e))
     }           
 }
